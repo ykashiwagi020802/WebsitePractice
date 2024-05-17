d2d140c9fc0b8c1f9bbcf8bbd2a2cf25d99fb415
yusuke.kashiwagi <ykashiwagi020802@yahoo.co.jp>
2024-05-17 23:03:41 +0900

    修正後_再納品

--- Kira-Kira Juku/js/main.js
+++ Kira-Kira Juku/js/main.js
@@ -1,41 +1,32 @@
 // ハンバーガーメニュー
-var hamburger = $('.hamburger-menu');
+var hamburger = $('.hamburger--menu');
 // OPEN/CLOSEボタンをクリックしたら
-$('.hamburger-button').on('click', function () {
+$('.hamburger--button').on('click', function () {
   // console.log('click');
   // .hamburgerの表示・非表示を繰り返す
-  hamburger.toggleClass('hamburger-menu-active');
+  hamburger.toggleClass('hamburger--menu--active');
 });
 // 画面幅のサイズが変わったら
 $(window).on('resize', function () {
-  // console.log('resize');
+
   // ハンバーガーメニューを閉じる
-  hamburger.removeClass('hamburger-menu-active');
+  hamburger.removeClass('hamburger--menu--active');
 });
 
-// $('.js-fadein-left').waypoint({
-//   handler: function(direction) {
-
-//     if (direction === 'down') {
-//       $(this.element)
-//         .addClass('animate__fadeInLeft');
-//     }
-//   },
-//   offset: '50%',
-// });
-
-// $('.js-fadein-right').waypoint({
-//   handler: function(direction) {
-
-//     if (direction === 'down') {
-//       $(this.element)
-//         .addClass('animate__fadeInRight');
-//     }
-//   },
-//   offset: '50%',
-// });
 
 $('#smarttab').smartTab({
   autoAdjustHeight: false,
   enableUrlHash: false
 });
+
+$(function() {
+  $('.js--scroll').on('click',(e) => {
+    e.preventDefault();
+    let $target = $($(e.currentTarget).attr('href'));
+    if ($target.length > 0) {
+      $('html, body').animate({
+        scrollTop: $target.offset().top
+      }, 500, 'swing');
+    }
+  })
+})
