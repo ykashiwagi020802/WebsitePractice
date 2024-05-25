d915a7649708fc8ef22db47a23539a30d3359851
yusuke.kashiwagi <ykashiwagi020802@yahoo.co.jp>
2024-05-26 00:15:40 +0900

    LP2課題提出

--- LP_2/js/main.js
+++ LP_2/js/main.js
@@ -1,30 +1,3 @@
-// $(window).on('load resize', function(){
-//     var winW = $(window).width();
-//     var devW = 767;
-//     if (winW <= devW) {
-//        // スマホ用（767px以下）のスクリプトはここに
-//        $('.voice-items').slick();
-//     } else {
-//   // PC用（768px以上）のスクリプトはここに
-//   $('.voice-items').slick({
-//     infinite: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 2
-//   });
-//     }
-//   });
-  
-
-$('.voice-slick').slick({
-  infinite: true,
-  dots: true,
-  arrows: true,
-  prevArrow: '<img src="../assets/image/arrow_left_circle_sp_01.png" class="slide-arrow prev-arrow">',
-  nextArrow: '<img src="../assets/image/arrow_right_circle_sp_01.png" class="slide-arrow next-arrow">',
-  fade: true
-});
-
-
 $('.accordion-title').on('click', function(e) {
   var mark = $(this).find("i");
   mark.toggleClass("change-parts-rotate");
@@ -32,25 +5,24 @@ $('.accordion-title').on('click', function(e) {
   content.slideToggle();
 });
 
-// 省略
-// カルーセル
-// $('.cover-slick').slick({
-//   infinite: true,
-//   dots: true,
-//   arrows: true,
-//   prevArrow: '<img src="assets/image/arrow_left_circle_sp_01.png" class="slide-arrow prev-arrow">',
-//   nextArrow: '<img src="assets/image/arrow_right_circle_sp_01.png" class="slide-arrow next-arrow">',
-//   fade: true
-// });
-
-
 $('.cover-slick').slick({
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 2,
+  slidesToScroll: 3,
   dots: true,
   arrows: true,
+  centerPadding: '200px',
   prevArrow: '<img src="assets/image/arrow_left_circle_sp_01.png" class="slide-arrow prev-arrow">',
   nextArrow: '<img src="assets/image/arrow_right_circle_sp_01.png" class="slide-arrow next-arrow">',
-  fade: true
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
 });
+
+
