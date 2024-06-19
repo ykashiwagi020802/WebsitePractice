08d32fcbb96704f090617e3ac5c90e3ec2e84991
yusuke.kashiwagi <ykashiwagi020802@yahoo.co.jp>
2024-06-19 10:28:00 +0900

    Co-authored-by: NaokoMasuda <NaokoMasuda@users.noreply.github.com>

---
+++ LP_4/js/main.js
@@ -0,0 +1,21 @@
+// ハンバーガーメニュー
+var hamburger = $('.hamburger-menu');
+// OPEN/CLOSEボタンをクリックしたら
+$('.hamburger-button').on('click', function () {
+  // console.log('click');
+  // .hamburgerの表示・非表示を繰り返す
+  hamburger.toggleClass('hamburger-menu-active');
+});
+// 画面幅のサイズが変わったら
+$(window).on('resize', function () {
+
+  // ハンバーガーメニューを閉じる
+  hamburger.removeClass('hamburger-menu-active');
+});
+
+$('.gallary__photo--slick').slick({ 
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 3,
+  dots: true,
+});
